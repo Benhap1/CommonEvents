c0e0f45d91bd6d83c090ef0edaa769c10c55a973
Bakhtiyar Bobokulov <b.bobokulov@outlook.com>
2024-10-31 14:52:39 +0300

    feat: role for UserEvent added 4

--- src/main/java/ru/skillbox/common/events/account/UserEvent.java
+++ src/main/java/ru/skillbox/common/events/account/UserEvent.java
@@ -1,7 +1,14 @@
 package ru.skillbox.common.events.account;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEvent {
 
     private UUID id;
@@ -13,95 +20,4 @@ public class UserEvent {
     private String messagePermission;
     private boolean deleted;
     private boolean blocked;
-
-    // Пустой конструктор
-    public UserEvent() {
-    }
-
-    // Конструктор с параметрами
-    public UserEvent(UUID id, String firstName, String lastName, String email, String password,
-                     String role, String messagePermission, boolean deleted, boolean blocked) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.messagePermission = messagePermission;
-        this.deleted = deleted;
-        this.blocked = blocked;
-    }
-
-    // Геттеры и сеттеры
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getMessagePermission() {
-        return messagePermission;
-    }
-
-    public void setMessagePermission(String messagePermission) {
-        this.messagePermission = messagePermission;
-    }
-
-    public boolean isDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
-    }
-
-    public boolean isBlocked() {
-        return blocked;
-    }
-
-    public void setBlocked(boolean blocked) {
-        this.blocked = blocked;
-    }
 }
